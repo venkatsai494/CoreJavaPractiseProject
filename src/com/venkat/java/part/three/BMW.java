3c36f87cfce2ab6844dec666afb9bb1e89c1c2af
venkatsai494 <venkatsai494@gmail.com>
2024-05-24 15:27:49 +0530

    Annotations usage, specifically override annotation

--- src/com/venkat/java/part/three/BMW.java
+++ src/com/venkat/java/part/three/BMW.java
@@ -2,6 +2,18 @@ package com.venkat.java.part.three;
 
 public class BMW extends Car{//This is also called as has-a relationship/Child will extend Parent
 
+    /*
+    1. Compile-Time Checking: The primary function of the @Override annotation is to inform the compiler
+    that a method is intended to override a method in a superclass. This enables the compiler to perform
+    checks and ensure that the method is indeed overriding a method from its superclass.
+    2. Error Prevention: Using @Override helps prevent subtle bugs caused by incorrectly matching method
+     signatures or method names. If a method is intended to override a method in the superclass but doesn't
+     do so due to a typo or incorrect signature, the compiler will alert you.
+    3.Code Readability: The @Override annotation serves as documentation, making it clear to anyone reading the code
+    that a method is intended to override a method in the superclass. This enhances code readability and
+    helps other developers understand the code more quickly.
+    */
+    @Override
     public void start() {
         System.out.println("BMW-----Start");
     }
