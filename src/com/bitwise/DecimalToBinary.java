07acddf2be0801fa829737a0ec43c31d4b831bfd
venkatsai494 <venkatsai494@gmail.com>
2024-07-03 12:15:46 +0530

    Conversion of Decimal to Binary

---
+++ src/com/bitwise/DecimalToBinary.java
@@ -0,0 +1,9 @@
+package com.bitwise;
+
+public class DecimalToBinary {
+    int decimalNumber = 50;
+    public static void main(String[] args) {
+        DecimalToBinary dtb = new DecimalToBinary();
+        System.out.println(Integer.toBinaryString(dtb.decimalNumber));
+    }
+}
