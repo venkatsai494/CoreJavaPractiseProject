71afe24495d24abb7bc385a27ca4fa988ededaec
venkatsai494 <venkatsai494@gmail.com>
2024-08-23 10:38:04 +0530

    Invocation of Builder design patterns with House class

--- src/com/designpatterns/creational/builder/BuilderDesignPattern.java
+++ src/com/designpatterns/creational/builder/BuilderDesignPattern.java
@@ -1,4 +1,13 @@
 package com.designpatterns.creational.builder;
 
 public class BuilderDesignPattern {
+
+    public static void main(String[] args) {
+        House h1 = new House.HouseBuilder()
+                        .buildFoundation("Laying foundation")
+                        .buildWalls("Buliding walls")
+                        .buildRoof("Buliding roof by laying slab")
+                        .build();
+
+    }
 }
