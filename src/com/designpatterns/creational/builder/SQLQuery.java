e3886ceff00f1625c5f53496b603bbcf7e1bd147
venkatsai494 <venkatsai494@gmail.com>
2024-09-02 09:31:12 +0530

    Adding select in SQLQuery class

--- src/com/designpatterns/creational/builder/SQLQuery.java
+++ src/com/designpatterns/creational/builder/SQLQuery.java
@@ -1,24 +1,43 @@
 package com.designpatterns.creational.builder;
 
 public class SQLQuery {
+    private String select;
     private String table;
     private String whereClause;
     private String orderByClause;
     private int limit;
 
     private SQLQuery(SQLQueryBuilder builder) {
+        this.select = builder.select;
         this.table = builder.table;
         this.whereClause = builder.whereClause;
         this.orderByClause = builder.orderByClause;
         this.limit = builder.limit;
     }
 
+    @Override
+    public String toString(){
+        StringBuilder query = new StringBuilder();
+        if(select!=null)
+            query.append("SELECT ").append(select).append(" ");
+        if(table!=null)
+            query.append("FROM ").append(table).append(" ");
+        if(whereClause!=null)
+            query.append("WHERE ").append(table).append(" ");
+        return query.toString().trim();
+    }
+
     public static class SQLQueryBuilder {
+        private String select;
         private String table;
         private String whereClause;
         private String orderByClause;
         private int limit;
 
+        public SQLQueryBuilder setSelect(String select) {
+            this.select = select;
+            return this;
+        }
         public SQLQueryBuilder setTable(String table) {
             this.table = table;
             return this;
