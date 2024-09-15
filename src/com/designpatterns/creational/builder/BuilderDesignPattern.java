b7a2dca3894c3a5fda2f842df2f0bc90d9918674
venkatsai494 <venkatsai494@gmail.com>
2024-09-15 17:35:24 +0530

    Added few more lines of code in BuilderDesignPattern.java

--- src/com/designpatterns/creational/builder/BuilderDesignPattern.java
+++ src/com/designpatterns/creational/builder/BuilderDesignPattern.java
@@ -1,18 +1,27 @@
 package com.designpatterns.creational.builder;
 
+
+
 public class BuilderDesignPattern {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IllegalArgumentException {
         House h1 = new House.HouseBuilder()
                         .buildFoundation("Laying foundation")
                         .buildWalls("Buliding walls")
                         .buildRoof("Buliding roof by laying slab")
                         .build();
+        System.out.println(h1.toString());
+
+        SQLQuery sql = new SQLQuery.SQLQueryBuilder()
+                .setSelect("EMP_ID, EMP_SALARY")
+                .setFromClause("EMPLOYEE")
+                .setWhereClause("SALARY>175000")
+                .setSelect("EMPI_ID")
+                .setOrderByClause("EMP_ID")
+                .setLimit(20)
+                .build();
+
+        System.out.println(sql.toString());
 
     }
-
-    SQLQuery sql = new SQLQuery.SQLQueryBuilder()
-            .setSelect("EMP_ID")
-            .setTable("EMPLOYEE")
-            .setWhereClause()
 }
