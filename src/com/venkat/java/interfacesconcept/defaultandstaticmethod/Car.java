63558aa69a9ce786c6e00155b7ea33515f4957be
venkatsai494 <venkatsai494@gmail.com>
2024-09-26 09:31:41 +0530

    Adding the multiple inheritance example using Interfaces

--- src/com/venkat/java/interfacesconcept/defaultandstaticmethod/Car.java
+++ src/com/venkat/java/interfacesconcept/defaultandstaticmethod/Car.java
@@ -1,6 +1,6 @@
 package com.venkat.java.interfacesconcept.defaultandstaticmethod;
 
-public class Car implements Vehicle {
+public class Car implements Vehicle, Automobile {
     @Override
     public void aMethod1() {
         System.out.println("Abstract Method is implemented here");
@@ -9,5 +9,10 @@ public class Car implements Vehicle {
     @Override
     public void dMethod2(){
         System.out.println("Default method overidden");
+    }
+
+    @Override
+    public void travel() {
+        System.out.println("Travelling as it is automobile");
     }
 }
