6fe338da249f41406d5649ec1084ed393ffb300d
venkatsai494 <venkatsai494@gmail.com>
2024-09-27 09:08:37 +0530

    Testing Multiple Inheritance using Interfaces and also checking ISP principle from SOLID

--- src/com/venkat/java/interfacesconcept/defaultandstaticmethod/Demo.java
+++ src/com/venkat/java/interfacesconcept/defaultandstaticmethod/Demo.java
@@ -8,6 +8,14 @@ public class Demo {
         i.dMethod2();//Interface's default method can also be called directly
         i.dMethod3();//This is not overidden in implementing class hence it would be taken directly from interface
         Vehicle.sMethod4();//The static methods are not inherited by implementing classes and cannot be overridden
+
+        Automobile a = new Car();//As Car is implementing both interfaces, Vehicle and Automobile, we can see that the object of Car can be referred by either of the Interface type (either Automobile or Vehicle), but point to be noted is that if it is referred by Car then it cannot access the exlusive methods of Vehicle Interface
+        a.travel();
+        //a.aMethod1();//This is not allowed as aMethod1 is the method of Interface Vehicle but the reference type of a is of Automobile. So in order to call aMethod1() we can do as follow
+        ((Vehicle) a).aMethod1();//This is called typecasting the
+
+
+        //This concept relates to the ISP from SOLID principles, which states that clients should not be forced to depend on interfaces they do not use. In your case, Vehicle and Automobile are separate interfaces, and when you use Vehicle v1 = new Car();, you're adhering to the principle by interacting with Car through the Vehicle interface.
     }
 
 }
